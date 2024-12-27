1f33a1a80b27e11797da86c0269379e5b8dde1ca
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-12-27 04:28:28 +0200

    done some failure next_round

---
+++ next_round.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, k;
+
+	cin >> n >> k;
+
+	int a;
+	int count = 0;
+	for (int i = 0; i < k; i++) {
+		cin >> a;
+		if (a == 0) {
+			cout << i << endl;
+			return 0;
+		}
+	}
+	if (k == n) {
+		cout << k << endl;
+		return 0;
+	}
+	int ref;
+	cin >> ref;
+	for (int i = k; i < n; i++) {
+		cin >> a;
+		if (a < ref) {
+			cout << k + (i - k) << endl;
+			return 0;
+		}
+	}
+	cout << n;
+}
